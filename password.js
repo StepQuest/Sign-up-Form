129242b95e43d249994954cddf7e3a77ea31f429
Baltiemora <137335097+Baltiemora@users.noreply.github.com>
2024-02-06 18:51:42 +0200

    Add JS strings to disable button if passwords aren't the same

--- password.js
+++ password.js
@@ -6,6 +6,7 @@ let passwordConfirmation;
 let firstInput = document.querySelector("#password");
 let secondInput = document.querySelector("#confirm-password");
 let wrongAlert = document.querySelector("#wrong-password-match");
+let createAccoutBtn = document.querySelector("button");
 
 // Functions
 
@@ -14,10 +15,12 @@ function showReaction() {
         wrongAlert.style.visibility = 'visible';
         firstInput.style.borderColor = 'var(--incorrect-color)';
         secondInput.style.borderColor = 'var(--incorrect-color)';
+        createAccoutBtn.setAttribute('disabled', '');
     } else {
         wrongAlert.style.visibility = 'hidden';
         firstInput.style.borderColor = 'rgb(226, 222, 222)';
         secondInput.style.borderColor = 'rgb(226, 222, 222)';
+        createAccoutBtn.removeAttribute('disabled', '');
     }
 }
 
